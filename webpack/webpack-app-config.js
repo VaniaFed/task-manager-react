5b07d8623b49502435a2fe9194841040fbf1fdc7
vaniafed <ztankist@gmail.com>
2023-08-07 17:32:24 +0300

    fix

--- webpack/webpack-app-config.js
+++ webpack/webpack-app-config.js
@@ -1,12 +1,13 @@
+const path = require('path');
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const StylelintPlugin = require('stylelint-webpack-plugin');
 
-const path = require('path');
-
 module.exports = {
 	output: {
 		path: path.resolve(__dirname, '../dist'),
+		publicPath: '/',
 		clean: true,
 		filename: '[name].js',
 		assetModuleFilename: 'assets/[name][ext]',
